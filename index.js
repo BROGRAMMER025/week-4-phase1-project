d8f1ba51c474687c9534efb2bad96721fe06f7b2
Allan Kinyua Murigi <allan.murigi@student.moringaschool.com>
2023-10-25 00:11:26 +0300

    third commit

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const busUrl = 'https://api.wheretheiss.at/v1/satellites'
-async function  getLoc(){
-  const response = await fetch('https://api.wheretheiss.at/v1/satellites')
-  const data = await response.json()
-  console.log(data)
-}
-getLoc()
+// Define the API URL
+const apiUrl = 'airport-web.appspot.com/_ah/api ';
+
+// Make a GET request using the fetch API
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json(); // Assuming the response is in JSON format
+  })
+  .then(data => {
+    // Process the API response data here
+    console.log('API Response:', data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
