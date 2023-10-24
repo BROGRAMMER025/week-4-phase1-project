5344ed428a105494b5f68eefc55648ee2980e7b3
Allan Kinyua Murigi <allan.murigi@student.moringaschool.com>
2023-10-24 20:37:54 +0300

    second commit

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-//transport services
-const axios = require('axios');
-
-// Your API key
-const apiKey = jZWuIvXD5d9f0fGGe0wFAQ;
-
-// API endpoint
-const apiUrl = 'https://bus-api.blablacar.com'
-
-// Create headers with the API key
-const headers = {
-  Authorization: `Bearer ${apiKey}`
-};
-
-// Make a GET request to the API
-axios.get(apiUrl, { headers })
-  .then(response => {
-    // Handle the API response
-    console.log('API Response:', response.data);
-  })
-  .catch(error => {
-    // Handle errors
-    console.error('API Error:', error);
-  });
+const busUrl = 'https://api.wheretheiss.at/v1/satellites'
+async function  getLoc(){
+  const response = await fetch('https://api.wheretheiss.at/v1/satellites')
+  const data = await response.json()
+  console.log(data)
+}
+getLoc()
